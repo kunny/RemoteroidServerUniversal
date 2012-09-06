1ec934687e6f3db7bf77676ab14ed7d2e8c97e0e
Taeho Kim <jyte82@gmail.com>
2012-09-07 01:11:44 +0900

    Added command for communication between device

--- RemoteroidServerUniversal_lib/src/org/secmem/remoteroid/lib/net/CommandPacket.java
+++ RemoteroidServerUniversal_lib/src/org/secmem/remoteroid/lib/net/CommandPacket.java
@@ -5,13 +5,116 @@ import java.util.HashMap;
 import com.google.gson.Gson;
 
 public class CommandPacket {
+	/**
+	 * Socket port number for command.
+	 */
 	public static final int SOCKET_PORT = 55001;
-	
-	public static final int CMD_INIT_CONNECTION = 0;
-	
+
 	private static final String KEY_COMMAND = "command";
-	private static final String KEY_SCREEN_WIDTH = "width";
-	private static final String KEY_SCREEN_HEIGHT = "height";
+
+	
+	public static class Command{
+
+		/**
+		 * Command for 'Request device information'.<br/>
+		 * Sent from <b>Server</b>.
+		 */
+		public static final int REQUEST_DEVICE_INFO = 0x001;
+		
+		/**
+		 * Command for 'Send key up message'.<br/>
+		 * Sent from <b>Server</b>.
+		 * @see CommandPacket.Extra#KEYCODE
+		 */
+		public static final int KEY_UP = 0x002;
+		
+		/**
+		 * Command for 'Send key down message'.<br/>
+		 * Sent from <b>Server</b>.
+		 * @see CommandPacket.Extra#KEYCODE
+		 */
+		public static final int KEY_DOWN = 0x003;
+		
+		/**
+		 * Command for 'Send touch down message'.<br/>
+		 * Sent from <b>Server</b>.
+		 * @see CommandPacket.Extra#KEY_TOUCH_X
+		 * @see CommandPacket.Extra#KEY_TOUCH_Y
+		 */
+		public static final int TOUCH_DOWN = 0x003;
+		
+		/**
+		 * Command for 'Send touch up message'.<br/>
+		 * Sent from <b>Server</b>.
+		 * @see CommandPacket.Extra#KEY_TOUCH_X
+		 * @see CommandPacket.Extra#KEY_TOUCH_Y
+		 */
+		public static final int TOUCH_UP = 0x004;
+		
+		/**
+		 * Command for 'Device information'.
+		 * Sent from <b>Client</b>.
+		 * @see CommandPacket.Extra#KEY_SCREEN_WIDTH
+		 * @see CommandPacket.Extra#KEY_SCREEN_HEIGHT
+		 */
+		public static final int DEVICE_INFO = 0x100;
+		
+		/**
+		 * Command for 'Show/Handle notification'.<br/>
+		 * Sent from <b>Client</b>.
+		 * @see CommandPacket.Extra#KEY_NOTIFICATION_DATA1
+		 * @see CommandPacket.Extra#KEY_NOTIFICATION_DATA2
+		 * @see CommandPacket.Extra#KEY_NOTIFICATION_DATA3
+		 */
+		public static final int NOTIFICATION = 0x101;
+		
+		/**
+		 * Command for 'Disconnect connection'.<br/>
+		 * Sent from both <b>Server or Client</b>.
+		 */
+		public static final int DISCONNECT = 0x200;
+	}
+	
+	public static class Extra{
+		/**
+		 * Type : int
+		 */
+		public static final String KEY_SCREEN_WIDTH = "width";
+		/**
+		 * Type : int
+		 */
+		public static final String KEY_SCREEN_HEIGHT = "height";
+
+		/**
+		 * Type ; int
+		 */
+		public static final String KEY_KEYCODE = "keycode";
+		/**
+		 * Type : int
+		 */
+		public static final String KEY_TOUCH_X = "touch_x";
+		/**
+		 * Type : int
+		 */
+		public static final String KEY_TOUCH_Y = "touch_y";
+		
+		/**
+		 * Type : int
+		 * @see #NOTIFICATION_TYPE_GENERAL
+		 * @see #NOTIFICATION_TYPE_KAKAOTALK
+		 * @see #NOTIFICATION_TYPE_PHONE
+		 * @see #NOTIFICATION_TYPE_SMS
+		 */
+		public static final String KEY_NOTIFICATION_TYPE = "noti_type";
+		public static final String KEY_NOTIFICATION_DATA1 = "noti_d1";
+		public static final String KEY_NOTIFICATION_DATA2 = "noti_d2";
+		public static final String KEY_NOTIFICATION_DATA3 = "noti_d3";
+
+		public static final String NOTIFICATION_TYPE_PHONE = "phone";
+		public static final String NOTIFICATION_TYPE_SMS = "sms";
+		public static final String NOTIFICATION_TYPE_KAKAOTALK = "kakao";
+		public static final String NOTIFICATION_TYPE_GENERAL = "gen";
+	}
 	
 	private HashMap<String, String> d;
 	
@@ -26,6 +129,11 @@ public class CommandPacket {
 	
 	public CommandPacket addExtra(String key, int data){
 		d.put(key, new Integer(data).toString());
+		return this;
+	}
+	
+	public CommandPacket addExtra(String key, String data){
+		d.put(key, data);
 		return this;
 	}
 	
@@ -51,7 +159,7 @@ public class CommandPacket {
 	public int getIntExtra(String key){
 		String value = d.get(key);
 		if(value==null)
-			throw new IllegalStateException("Data has not been set.");
+			throw new IllegalStateException("Data has not been set for key "+key);
 		int commandAsInt;
 		try{
 			commandAsInt = Integer.parseInt(value);
@@ -62,11 +170,78 @@ public class CommandPacket {
 		return commandAsInt;
 	}
 	
-	public static class Builder{
-		CommandPacket sendInitialData(int width, int height){
-			return new CommandPacket().setCommand(CMD_INIT_CONNECTION)
-					.addExtra(KEY_SCREEN_WIDTH, width)
-					.addExtra(KEY_SCREEN_HEIGHT, height);
+	public String getStringExtra(String key){
+		String value = d.get(key);
+		if(value==null){
+			throw new IllegalStateException("Data has not been set for key "+key);
+		}
+		return value;
+	}
+	
+	public static class CommandFactory{
+		
+		CommandPacket requestDeviceInfo(){
+			return new CommandPacket().setCommand(Command.REQUEST_DEVICE_INFO);
+		}
+		
+		CommandPacket sendDeviceInfo(int width, int height){
+			return new CommandPacket().setCommand(Command.DEVICE_INFO)
+					.addExtra(Extra.KEY_SCREEN_WIDTH, width)
+					.addExtra(Extra.KEY_SCREEN_HEIGHT, height);
+		}
+		
+		CommandPacket keyDown(int keyCode){
+			return new CommandPacket().setCommand(Command.KEY_DOWN)
+					.addExtra(Extra.KEY_KEYCODE, keyCode);
+		}
+		
+		CommandPacket keyUp(int keyCode){
+			return new CommandPacket().setCommand(Command.KEY_UP)
+					.addExtra(Extra.KEY_KEYCODE, keyCode);
+		}
+		
+		CommandPacket touchDown(int x, int y){
+			return new CommandPacket().setCommand(Command.TOUCH_DOWN)
+					.addExtra(Extra.KEY_TOUCH_X, x).addExtra(Extra.KEY_TOUCH_Y, y);
+		}
+		
+		CommandPacket touchUp(int x, int y){
+			return new CommandPacket().setCommand(Command.TOUCH_UP)
+					.addExtra(Extra.KEY_TOUCH_X, x).addExtra(Extra.KEY_TOUCH_Y, y);
+		}
+		
+		CommandPacket notification(int notificationType, String... data){
+			CommandPacket packet = new CommandPacket();
+			packet.setCommand(Command.NOTIFICATION);
+			packet.addExtra(Extra.KEY_NOTIFICATION_TYPE, notificationType);
+			
+			if(data==null)
+				throw new IllegalArgumentException();
+			
+			int dataLen = data.length;
+			
+			switch(dataLen){
+			case 1:
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA1, data[0]);
+				break;
+			
+			case 2:
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA1, data[0]);
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA2, data[1]);
+				break;
+			
+			case 3:
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA1, data[0]);
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA2, data[1]);
+				packet.addExtra(Extra.KEY_NOTIFICATION_DATA3, data[2]);
+				break;
+			}
+			
+			return packet;
+		}
+		
+		CommandPacket disconnect(){
+			return new CommandPacket().setCommand(Command.DISCONNECT);
 		}
 	}
 	
